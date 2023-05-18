ff13fe430ba885917bbdab0213f81af0733a7f89
timeispreciousFeng <40915799+timeispreciousFeng@users.noreply.github.com>
2023-05-18 20:38:38 +0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -8,3 +8,6 @@ UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
 
 curl -H "Accept-Language: zh-CN"  -o result.html wttr.in/wuhan
+
+echo '<br/>表格click <A HREF="https://wttr.in/wuhan">here</A>.' >> result.html
+echo '<br/>折线click <A HREF="https://wttr.in/wuhan?format=v2">here</A>.' >> result.html
