ebf720b55bded4664d59422ef0cfcead345c3be9
timeispreciousFeng <40915799+timeispreciousFeng@users.noreply.github.com>
2023-05-18 20:47:18 +0800

    Update weather-hour.sh

--- weather-hour.sh
+++ weather-hour.sh
@@ -7,7 +7,7 @@ LANGUAGE="zh-CN"
 UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
 
-curl -H "Accept-Language: zh-CN"  -o result.html 'wttr.in/{wuhan,Shanghai}?format=4&m'
+curl -H "Accept-Language: zh-CN"  -o result.html 'wttr.in/wuhan?format=4&m'
 
 echo '<br/>表格click <A HREF="https://wttr.in/wuhan">here</A>.' >> result.html
 echo '<br/>折线click <A HREF="https://wttr.in/wuhan?format=v2">here</A>.' >> result.html
